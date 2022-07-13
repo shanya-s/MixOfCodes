7c3d356dbb07e521096f87d59d3a15680b24d40a
shanya-s <109117036+shanya-s@users.noreply.github.com>
2022-07-13 13:11:54 +1000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,19 @@
 input.onButtonPressed(Button.A, function () {
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(494, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Whole))
     music.playTone(392, music.beat(BeatFraction.Whole))
-    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
     music.playTone(262, music.beat(BeatFraction.Whole))
     music.playTone(262, music.beat(BeatFraction.Whole))
     music.playTone(262, music.beat(BeatFraction.Whole))
     music.playTone(262, music.beat(BeatFraction.Whole))
     music.playTone(262, music.beat(BeatFraction.Whole))
 })
-input.onGesture(Gesture.LogoUp, function () {
-    music.playMelody("F E F G - E D C ", 120)
+basic.forever(function () {
+    basic.showString("TWICE!")
 })
-input.onGesture(Gesture.ScreenDown, function () {
-    music.playMelody("B A B C5 - A G F ", 120)
-})
-input.onGesture(Gesture.Shake, function () {
-    music.playMelody("G F G A - F E D ", 120)
-})
-basic.showLeds(`
-    # . # . #
-    . # # # .
-    # # # # #
-    . # # # .
-    # . # . #
-    `)
